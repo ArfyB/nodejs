c7bcfe7fe98211c0e2e9a30c30a2a5b4c49aacb6
Arfy <arfybundle@gmail.com>
2023-09-10 21:00:42 +0900

    'a'

--- app.js
+++ app.js
@@ -1,20 +1,52 @@
-const http = require("http");
+const fs = require('fs');  // file system
+const path = require('path');
 
-function handleRequest(request, response) {
-  // localhost:3000/currenttime
-  ///currenttime부분
-  if (request.url === "/currenttime") {
-    response.statusCode = 200;
-    // new Date().toISOString() = 현재시간 출력
-    response.end("<h1>" + new Date().toISOString() + "<h1>");
-  } else if (request.url === "/") {
-    // localhost:3000
-    response.statusCode = 200; // 요청이 성공했는지 알리는 코드
-    response.end("<h1>Hello World!<h1>"); // 보내야 하는 데이터를 전달 가능.
+const express = require("express");
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/currenttime", function (req, res) {
+  res.send("<h1>" + new Date().toISOString() + "<h1>");
+}); // localhost:3000/currenttime
+
+app.get("/", function(req, res) {
+  res.send(
+    "<form action='/store-user' method='POST'><label>Your Name: </label><input type='text' name='username'><button>Submit</button></form>"
+  );  
+}); // localhost:3000/
+
+app.post("/store-user", function(req, res) {
+  const userName = req.body.username;
+
+  const filePath = path.join(__dirname, 'data', 'users.json');
+  // __dirname = 프로젝트 디렉토리에 대한 절대 경로를 실제로 보유하는 내장된 변수
+
+  const fileData = fs.readFileSync(filePath); // 데이터 텍스트화
+  const existingUsers = JSON.parse(fileData);  // 자바스크립트 객체, 배열로 변환
+
+  existingUsers.push(userName); // 추가할 데이터
+
+  fs.writeFileSync(filePath, JSON.stringify(existingUsers)); // 경로, 데이터  // 데이터 저장.
+
+  res.send("<h1>Username stored!</h1>");
+});
+
+app.get('/users', function(req, res) {
+  const filePath = path.join(__dirname, 'data', 'users.json');
+
+  const fileData = fs.readFileSync(filePath);
+  const existingUsers = JSON.parse(fileData);
+
+  let responseData = '<ul>';
+
+  for (const user of existingUsers) {
+    responseData += '<li>' + user + '</li>';
+    responseData += '</ul>';
   }
-}
 
-const server = http.createServer(handleRequest); // 들어오는 요청을 받았을때 handleRequest 실행
+  res.send(existingUsers);
+})
 
-server.listen(3000); // 포트 번호    주소창에 localhost:3000으로 접속가능.
-// ctrl + c 입력시 서버 종료.
+app.listen(3000);
